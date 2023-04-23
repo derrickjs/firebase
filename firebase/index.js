8e512dd2962f9da9857f61c0cab65a1bc891daad
derrickjs <stahlman.derrick@gmail.com>
2023-04-23 16:47:33 -0400

    new commit

--- firebase/index.js
+++ firebase/index.js
@@ -4,6 +4,6 @@ const functions = require("firebase-functions");
 // // https://firebase.google.com/docs/functions/get-started
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
-// functions.logger.info("Hello logs!", {structuredData: true});
+//   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
